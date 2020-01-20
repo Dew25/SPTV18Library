688b4fba28b443eac113d04dab3bc3d4a346f522
juri.melnikov <juri.melnikov@ivkhk.ee>
2020-01-20 10:35:47 +0200

    Добавлен интерфейс Saveble и реализованы методы сохранения книг в базу

--- src/entity/Book.java
+++ src/entity/Book.java
@@ -6,13 +6,21 @@
 package entity;
 
 import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  *
  * @author user
  */
+@Entity
 public class Book implements Serializable{
-   
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String title;
     private String author;
     private int publishedYear;
@@ -20,6 +28,18 @@ public class Book implements Serializable{
     public Book() {
     }
 
+    public Book(String title, String author, int publishedYear) {
+        this.title = title;
+        this.author = author;
+        this.publishedYear = publishedYear;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
     public String getTitle() {
         return title;
     }
@@ -48,6 +68,45 @@ public class Book implements Serializable{
     public String toString() {
         return "Book{" + "title=" + title + ", author=" + author + ", publishedYear=" + publishedYear + '}';
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 83 * hash + Objects.hashCode(this.id);
+        hash = 83 * hash + Objects.hashCode(this.title);
+        hash = 83 * hash + Objects.hashCode(this.author);
+        hash = 83 * hash + this.publishedYear;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Book other = (Book) obj;
+        if (this.publishedYear != other.publishedYear) {
+            return false;
+        }
+        if (!Objects.equals(this.title, other.title)) {
+            return false;
+        }
+        if (!Objects.equals(this.author, other.author)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    
     
     
 }
